4e050543acc1900ef7a4fef67bc8229185e827de
Marti500 <165767798+MartiFuertes@users.noreply.github.com>
2024-04-25 09:05:44 +0200

    ns

--- Escenes/jugador.gd
+++ Escenes/jugador.gd
@@ -7,18 +7,14 @@ var direccio = Vector2(1, 1).normalized()
 var previous_angle = 0
 var velocitat_angular = 0.05
 var angle_anterior = 0
-#var angle_dict = {Vector2(PI / 8, 15 * PI / 8) : 'E', Vector2(15 * PI / 8, 13 * PI / 8) : 'SE', Vector2(13 * PI / 8, 11 * PI / 8) : 'S', Vector2(11 * PI / 8, 9 * PI / 8) : 'SW', Vector2(9 * PI / 8, 7 * PI / 8) : 'W', Vector2(7 * PI / 8, 5 * PI / 8) : 'NW', Vector2(5 * PI / 8, 3 * PI / 8) : 'N', Vector2(3 * PI/8, PI / 8) : 'NE'}
-var angle_dict = {0 : 'E', 7 * PI / 4 : 'SE', 3 * PI / 2 : 'S', 5 * PI / 4 : 'SW', PI : 'W', 3 * PI / 4 : 'NW', PI / 2 : 'N', PI / 4 : 'NE'}					
+var angle_dict = {Vector2(PI / 8, 15 * PI / 8) : 'E', Vector2(15 * PI / 8, 13 * PI / 8) : 'SE', Vector2(13 * PI / 8, 11 * PI / 8) : 'S', Vector2(11 * PI / 8, 9 * PI / 8) : 'SW', Vector2(9 * PI / 8, 7 * PI / 8) : 'W', Vector2(7 * PI / 8, 5 * PI / 8) : 'NW', Vector2(5 * PI / 8, 3 * PI / 8) : 'N', Vector2(3 * PI/8, PI / 8) : 'NE'}
+#var angle_dict = {(,): 'E', 7 * PI / 4 : 'SE', 3 * PI / 2 : 'S', 5 * PI / 4 : 'SW', PI : 'W', 3 * PI / 4 : 'NW', PI / 2 : 'N', PI / 4 : 'NE'}					
 #var resolucio = get_tree().root.content_scale_size
 
-func find_similar(angle, dict):
-	var most_similar = 100
-	
+func troba_animacio(angle, dict):
 	for item in dict:
-		if abs(item - angle) < most_similar:
-			most_similar = item
-	
-	return dict[most_similar]
+		if item.x >= angle and item.y <= angle:
+			return dict[item]
 
 func _ready():
 	#position = Vector2(resolucio.x / 2, resolucio.y / 2)
@@ -42,12 +38,14 @@ func _process(delta):
 	
 	angle = direccio.angle()
 	
-	print(find_similar(angle, angle_dict))
+	print(troba_animacio(angle, angle_dict))
 	
-	if find_similar(angle, angle_dict) == 'E':
-		$animacions/E.play()
-		
-	if find_similar(angle, angle_dict) == 'SE':
-		$animacions/SE.play()
+	$animacio.play(troba_animacio(angle, angle_dict))
+	
+	#if find_similar(angle, angle_dict) == 'E':
+		#$animacions/E.play()
+		#
+	#if find_similar(angle, angle_dict) == 'SE':
+		#$animacions/SE.play()
 
 	
