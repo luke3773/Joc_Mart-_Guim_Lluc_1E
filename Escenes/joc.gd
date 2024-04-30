29869717f40aec210dfd9cb5f8939cfccdda012b
Marti500 <165767798+MartiFuertes@users.noreply.github.com>
2024-04-30 10:05:36 +0200

    borrat algo

--- Escenes/joc.gd
+++ Escenes/joc.gd
@@ -3,13 +3,13 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$camera_multiple.add_target($lladre)
-	$camera_multiple.add_target($policia)
+#	$camera_multiple.add_target($lladre)
+#	$camera_multiple.add_target($policia)
 	
-	var r = $mapa.get_used_rect()
-	$camera_multiple.limit_left = r.position.x * $mapa.tile_set.tile_size.x
-	$camera_multiple.limit_right = r.end.x * $mapa.tile_set.tile_size.x
-
+#	var r = $mapa.get_used_rect()
+#	$camera_multiple.limit_left = r.position.x * $mapa.tile_set.tile_size.x
+#	$camera_multiple.limit_right = r.end.x * $mapa.tile_set.tile_size.x
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
