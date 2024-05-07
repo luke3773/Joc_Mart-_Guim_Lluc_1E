eefe2595e72582185a83bcc14821b17cc2266499
luke3773 <165813941+luke3773@users.noreply.github.com>
2024-05-07 09:48:46 +0200

    Menu pausa creat

---
+++ MenuPausa.gd
@@ -0,0 +1,26 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		visible = !visible
+		get_tree().paused = !get_tree().paused
+
+
+func _on_menu_principal_pressed():
+	get_tree().change_scene_to_file("res://Escenes/UI inicial.gd")
+
+
+func _on_sortir_pressed():
+	get_tree().quit()
+
+
+func _on_continua_pressed():
+	visible = false
+	get_tree().paused = false
