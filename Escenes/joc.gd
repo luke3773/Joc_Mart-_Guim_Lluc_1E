01b0b8eccb12fa564940c6e0fb5449695b0519fe
Marti500 <165767798+MartiFuertes@users.noreply.github.com>
2024-05-09 08:14:00 +0200

    Revert "mapa definitiu en joc.tsc"
    This reverts commit d3542ce692b18d4552c486c2711323be8021b736.

--- Escenes/joc.gd
+++ Escenes/joc.gd
@@ -6,9 +6,9 @@ func _ready():
 	$camera_multiple.add_target($lladre)
 	$camera_multiple.add_target($policia)
 	
-	var r = %MapaDefinitiu.get_used_rect()
-	$camera_multiple.limit_left = r.position.x * %MapaDefinitiu.tile_set.tile_size.x
-	$camera_multiple.limit_right = r.end.x * %MapaDefinitiu.tile_set.tile_size.x
+	var r = $mapa.get_used_rect()
+	$camera_multiple.limit_left = r.position.x * $mapa.tile_set.tile_size.x
+	$camera_multiple.limit_right = r.end.x * $mapa.tile_set.tile_size.x
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
