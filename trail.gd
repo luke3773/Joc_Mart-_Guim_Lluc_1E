32d3b2bc154bcce7fc08340f840db89c31a3fb72
Marti500 <165767798+MartiFuertes@users.noreply.github.com>
2024-05-10 14:19:23 +0200

    trail

--- trail.gd
+++ trail.gd
@@ -6,6 +6,7 @@ extends Node2D
 var points = []
 var frame = 0
 
+
 func _physics_process(delta):
 	if frame % 3 == 0:
 		points.push_front(global_position)
@@ -27,9 +28,9 @@ func _draw():
 	var colors = PackedColorArray()
 	
 	for i in range(s):
-		adjusted.append(points[1] - global_position)
+		adjusted.append(points[i] - global_position)
 		c.a = lerp(1.0, 0.0, i/s)
 		colors.append(c)
 
-	draw_set_transform(Vector2(0,0), -get_parent().rotation, Vector2(1,1))
+	draw_set_transform(Vector2(0,0), get_parent().rotation, Vector2(1,1))
 	draw_polyline_colors(adjusted, colors, max_width, antialias)
