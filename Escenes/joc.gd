5d26808bfe90eaaf8e31c21f2cdc86b2be2cd010
luke3773 <165813941+luke3773@users.noreply.github.com>
2024-05-11 18:34:45 +0200

    fletxa apunta a on toca

--- Escenes/joc.gd
+++ Escenes/joc.gd
@@ -13,4 +13,5 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if $lladre/VisibleOnScreenNotifier2D.is_on_screen() == false:
-		print('game over')
+		get_tree().change_scene_to_file("res://Escenes/EscenaFinalLladre.tscn")
+	pass
