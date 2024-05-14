aeb2b4cddf6c3ca2b2f604bd8f40d5fe0fa7f9c1
luke3773 <165813941+luke3773@users.noreply.github.com>
2024-05-14 16:00:16 +0200

    millores escenes game over

--- Escenes/EscenaFinalLladre.gd
+++ Escenes/EscenaFinalLladre.gd
@@ -3,9 +3,17 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Game_over.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_reiniciar_pressed():
+	get_tree().change_scene_to_file("res://Escenes/joc.tscn")
+
+
+func _on_sortir_pressed():
+	get_tree().quit()
